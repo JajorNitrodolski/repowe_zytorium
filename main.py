2889af41205af619fadf2054cd64ad5fee961121
JajorNitrodolski <sonic579x@gmail.com>
2024-02-25 15:01:00 +0100

    komit 2

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import sys
 print(sys.version)
 
 #git config --list
-#git config --global user.name JajorNitrodolski
-#git config --global user.email sonic579x@gmail.com
+#git config --global user.name
+#git config --global user.email
